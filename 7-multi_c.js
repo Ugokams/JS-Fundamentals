552a237b83ab7e70073218c36fbab4e127c776f4
Nellydasylva <ugokamslink@gmail.com>
2025-11-06 17:40:02 +0100

    7-multi_c.js has been updated to use only 2 console.log)

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,8 +1,5 @@
 const x = process.argv[2];
-if(isNaN(Number(x))){
-    console.log('Missing number of occurrences')
-}
-else if(Number(x) === undefined){
+if(isNaN(Number(x)) || (Number(x) === undefined)){
     console.log('Missing number of occurrences')
 }
 else{
