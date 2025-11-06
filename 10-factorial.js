600f870872e3f25b76d89c58705a46d8879b12c2
Nellydasylva <ugokamslink@gmail.com>
2025-11-06 23:58:26 +0100

    factorial of NaN has been updated on 10-factorial.js

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,6 +1,9 @@
 const num = Number(process.argv[2]);
 function factorial(n){
-    if (n === 0 || n === 1){
+    if (isNaN(n)){
+        return 1;
+    }
+    else if (n === 0 || n === 1){
         return 1;
     }
     else{
