0e4a7b2a39fe462c346fe21e4c32f5bbc3a043a4
Nellydasylva <ugokamslink@gmail.com>
2025-11-06 17:51:27 +0100

    7-multi_c.js has been updated to pass the auto-checks

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,9 +1,9 @@
 const x = process.argv[2];
-if(isNaN(Number(x)) || (Number(x) === undefined)){
+if (isNaN(Number(x)) || (Number(x) === undefined)){
     console.log('Missing number of occurrences')
 }
 else{
-    for(let i = 0; i < x; i++){
+    for (let i = 0; i < x; i++){
         console.log('C is fun');
     }
 }
