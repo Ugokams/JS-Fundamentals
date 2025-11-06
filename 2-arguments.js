170b79b89f2e42567fdb3cda6f36e1baea40259b
Nellydasylva <ugokamslink@gmail.com>
2025-11-06 12:04:39 +0100

    2-arguments.js has been added

---
+++ 2-arguments.js
@@ -0,0 +1,10 @@
+let numArguement = prompt('Enter the number of arguements: ');
+if (numArguement < 1){
+    console.log('No argument');
+}
+else if(numArguement == 1){
+    console.log('Argument found');
+}
+else{
+    console.log('Arguments found');
+}
